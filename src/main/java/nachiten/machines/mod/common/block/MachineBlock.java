344201b2141bb3d6c56e434eb14f37279d8c4054
Ignacio Baptista [Notebook] <ibaptista@frba.utn.edu.ar>
2021-07-08 10:43:47 -0300

    Agregado machine block

---
+++ src/main/java/nachiten/machines/mod/common/block/MachineBlock.java
@@ -0,0 +1,9 @@
+package nachiten.machines.mod.common.block;
+
+import net.minecraft.block.Block;
+
+public class MachineBlock extends Block {
+    public MachineBlock(Properties properties) {
+        super(properties);
+    }
+}
